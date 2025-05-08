36464db240eef878af6068e8c8f346a68c404910
Gomes, J.M <gomesjm@ita.br>
2025-05-08 15:37:40 -0300

    fix AsyncIterator usage

--- src/sikei/brokers/redis.py
+++ src/sikei/brokers/redis.py
@@ -9,12 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 class RedisMessageBroker:
-    def __init__(self, client: Redis, *, channel_prefix: str | None = None) -> None:
+    def __init__(self, client: callable([..., Redis]), *, prefix: str | None = None) -> None:
         self._client = client
-        self._channel_prefix = channel_prefix or "python_sikei_channel"
+        self._prefix = prefix or "python_sikei_channel"
 
     async def send(self, message: Message) -> None:
-        channel = f"{self._channel_prefix}:{message.message_type}:{message.message_id}"
-
+        channel = f"{self._prefix}:{message.message_type}:{message.message_id}"
         logger.debug("Sending message to Redis Pub/Sub %s.", message.message_id)
-        await self._client.publish(channel, orjson.dumps(message.model_dump()))
+        async for _ in self._client():
+            await _.publish(channel, orjson.dumps(message.model_dump()))
+            await _.aclose()
+            break
